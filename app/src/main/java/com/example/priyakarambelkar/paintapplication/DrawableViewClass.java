e043c6db863227a591b76dc021924326955637b5
priyakarambelkar <piyu004@gmail.com>
2015-06-10 08:56:58 -0400

    added round button to drawable

--- app/src/main/java/com/example/priyakarambelkar/paintapplication/DrawableViewClass.java
+++ app/src/main/java/com/example/priyakarambelkar/paintapplication/DrawableViewClass.java
@@ -1,26 +1,78 @@
 package com.example.priyakarambelkar.paintapplication;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by priyakarambelkar on 6/9/15.
  */
 public class DrawableViewClass extends View {
+
+    private Path drawingPath;
+    private Paint drawingPaint, drawingOnCanvas;
+    private Canvas drawingCanvas;
+    private Bitmap canvasBitmap = null;
+    int initColor = 0x0066FF;
+
     public DrawableViewClass(Context context, AttributeSet attrs) {
         super(context, attrs);
         startDrawingCanvas();
     }
 
     private void startDrawingCanvas() {
+        drawingPath = new Path();
+        drawingPaint = new Paint();
+        drawingPaint.setAntiAlias(true);
+        drawingPaint.setColor(Color.BLUE);
+        drawingPaint.setStrokeWidth(50);
+        drawingPaint.setStyle(Paint.Style.STROKE);
+        drawingPaint.setStrokeCap(Paint.Cap.ROUND);
+        drawingPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawingOnCanvas = new Paint (Paint.DITHER_FLAG);
 
     }
 
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w,h, Bitmap.Config.ARGB_8888);
+        drawingCanvas = new Canvas(canvasBitmap);
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        canvas.drawBitmap(canvasBitmap, 0, 0, drawingOnCanvas);
+        canvas.drawPath(drawingPath, drawingPaint);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float drawX = event.getX();
+        float drawY = event.getY();
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                drawingPath.moveTo(drawX,drawY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawingPath.lineTo(drawX, drawY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawingCanvas.drawPath(drawingPath, drawingPaint);
+                break;
+            default:
+                return false;
+        }
+        invalidate();
+        return true;
     }
 }
--- app/src/main/java/com/example/priyakarambelkar/paintapplication/MainActivityFragment.java
+++ app/src/main/java/com/example/priyakarambelkar/paintapplication/MainActivityFragment.java
@@ -5,19 +5,56 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
-
+    Button palette;
+    RadioGroup colors;
+    public boolean visible = false;
+    private DrawableViewClass drawableView;
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+               View view = inflater.inflate(R.layout.fragment_main, container, false);
+        initialiseParameters(view);
+        palette.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //setColorVisible();
+                if (visible) {
+                    setColorInvisible(v);
+                }
+                else if (!visible) {
+                    setColorVisible(v);
+                }
+            }
+        });
+        return view;
     }
+
+    private void initialiseParameters(View view) {
+        palette = (Button) view.findViewById(R.id.button);
+        drawableView = (DrawableViewClass) view.findViewById(R.id.drawCustomView);
+        colors = (RadioGroup)view.findViewById(R.id.radio_group);
+        colors.setVisibility(View.INVISIBLE);
+
+    }
+
+    private void setColorVisible(View view) {
+        colors.setVisibility(View.VISIBLE);
+        visible = true;
+    }
+
+   public void setColorInvisible(View view){
+       colors.setVisibility(View.INVISIBLE);
+       visible = false;
+   }
 }
